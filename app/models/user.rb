30dc4d04a41df21c5544ea619f6e0fa8cc843da6
jo aoki <jo@reactcorp.com>
2018-08-08 12:11:01 +0900

    Add Crypt Password to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ApplicationRecord
+  # setup
+  before_save {
+    self.email.downcase!
+  }
+
+  # name validation
   validates(:name, { presence: true, length: { maximum: 50 } })
 
+  # email validation
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates(:email, {
     presence: true,
     length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
+    })
+
+  # password validation
+  has_secure_password
+  validates(:password, {
+    presence: true,
+    length: { minimum: 6 }
     })
 end
