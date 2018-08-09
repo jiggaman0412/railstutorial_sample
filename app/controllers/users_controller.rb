e15636d2164dd53a4ef9d28b38a29708201b8941
jo aoki <jo@reactcorp.com>
2018-08-09 09:52:08 +0900

    Add flash & tweak

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #保存の成功
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
     else
       render 'new'
     end
